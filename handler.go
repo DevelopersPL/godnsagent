914af5f8791a038c803360a34546734682f72936
Daniel Speichert <daniel@speichert.pl>
2016-03-27 20:21:23 +0200

    Properly reply with CNAMES or NS delegation

--- handler.go
+++ handler.go
@@ -54,7 +54,7 @@ func handleDNS(w dns.ResponseWriter, req *dns.Msg) {
 		} else {
 			m := new(dns.Msg)
 			m.SetReply(req)
-			m.SetRcode(req, dns.RcodeNameError)
+			m.SetRcode(req, dns.RcodeRefused)
 			w.WriteMsg(m)
 		}
 		return
@@ -73,7 +73,7 @@ func handleDNS(w dns.ResponseWriter, req *dns.Msg) {
 		answerKnown = true
 	}
 
-	// check for a wildcarad record (*.zone)
+	// check for a wildcard record (*.zone)
 	if !answerKnown {
 		for _, r := range (*zone)[dns.RR_Header{Name: "*." + name, Rrtype: req.Question[0].Qtype, Class: req.Question[0].Qclass}] {
 			r.Header().Name = dns.Fqdn(req.Question[0].Name)
@@ -82,12 +82,33 @@ func handleDNS(w dns.ResponseWriter, req *dns.Msg) {
 		}
 	}
 
+	if !answerKnown {
+		// we don't have a direct response but may find alternative record type: CNAME
+		for _, r := range (*zone)[dns.RR_Header{Name: req.Question[0].Name, Rrtype: dns.TypeCNAME, Class: req.Question[0].Qclass}] {
+			m.Answer = append(m.Answer, r)
+			answerKnown = true
+		}
+	}
+
+	if !answerKnown {
+		// we don't have a direct response but may find alternative record type: NS
+		for _, r := range (*zone)[dns.RR_Header{Name: req.Question[0].Name, Rrtype: dns.TypeNS, Class: req.Question[0].Qclass}] {
+			m.Answer = append(m.Answer, r)
+			answerKnown = true
+		}
+	}
+
+	// now we try recursion if enabled
 	if !answerKnown && recurseTo != "" { // we don't want to handleFailed when recursion is disabled
 		recurse(w, req)
 		return
-	} else if !answerKnown {
+	}
+
+	if !answerKnown {
+		// we really don't have any record to offer
+		m.SetRcode(req, dns.RcodeNameError)
 		m.Ns = (*zone)[dns.RR_Header{Name: name, Rrtype: dns.TypeSOA, Class: dns.ClassINET}]
-	} else { // answerKnown
+	} else {
 		// Add Authority section
 		for _, r := range (*zone)[dns.RR_Header{Name: name, Rrtype: dns.TypeNS, Class: dns.ClassINET}] {
 			m.Ns = append(m.Ns, r)
@@ -103,33 +124,34 @@ func handleDNS(w dns.ResponseWriter, req *dns.Msg) {
 				}
 			}
 		}
+	}
 
-		// Resolve extra lookups for CNAMEs, SRVs, etc. and put in ADDITIONAL SECTION
-		for _, r := range m.Answer {
-			switch r.Header().Rrtype {
-			case dns.TypeCNAME:
-				zone2, _ := zones.match(r.(*dns.CNAME).Target, dns.TypeA)
-				if zone2 != nil {
-					for _, r := range (*zone2)[dns.RR_Header{Name: r.(*dns.CNAME).Target, Rrtype: dns.TypeA, Class: dns.ClassINET}] {
-						m.Extra = append(m.Extra, r)
-					}
-					for _, r := range (*zone2)[dns.RR_Header{Name: r.(*dns.CNAME).Target, Rrtype: dns.TypeAAAA, Class: dns.ClassINET}] {
-						m.Extra = append(m.Extra, r)
-					}
+	// Resolve extra lookups for CNAMEs, SRVs, etc. and put in ADDITIONAL SECTION
+	for _, r := range m.Answer {
+		switch r.Header().Rrtype {
+		case dns.TypeCNAME:
+			zone2, _ := zones.match(r.(*dns.CNAME).Target, dns.TypeA)
+			if zone2 != nil {
+				for _, r := range (*zone2)[dns.RR_Header{Name: r.(*dns.CNAME).Target, Rrtype: dns.TypeA, Class: dns.ClassINET}] {
+					m.Extra = append(m.Extra, r)
 				}
-			case dns.TypeSRV:
-				zone2, _ := zones.match(r.(*dns.SRV).Target, dns.TypeA)
-				if zone2 != nil {
-					for _, r := range (*zone2)[dns.RR_Header{Name: r.(*dns.SRV).Target, Rrtype: dns.TypeA, Class: dns.ClassINET}] {
-						m.Extra = append(m.Extra, r)
-					}
-					for _, r := range (*zone2)[dns.RR_Header{Name: r.(*dns.SRV).Target, Rrtype: dns.TypeAAAA, Class: dns.ClassINET}] {
-						m.Extra = append(m.Extra, r)
-					}
+				for _, r := range (*zone2)[dns.RR_Header{Name: r.(*dns.CNAME).Target, Rrtype: dns.TypeAAAA, Class: dns.ClassINET}] {
+					m.Extra = append(m.Extra, r)
+				}
+			}
+		case dns.TypeSRV:
+			zone2, _ := zones.match(r.(*dns.SRV).Target, dns.TypeA)
+			if zone2 != nil {
+				for _, r := range (*zone2)[dns.RR_Header{Name: r.(*dns.SRV).Target, Rrtype: dns.TypeA, Class: dns.ClassINET}] {
+					m.Extra = append(m.Extra, r)
+				}
+				for _, r := range (*zone2)[dns.RR_Header{Name: r.(*dns.SRV).Target, Rrtype: dns.TypeAAAA, Class: dns.ClassINET}] {
+					m.Extra = append(m.Extra, r)
 				}
 			}
 		}
 	}
+
 	m.Answer = dns.Dedup(m.Answer, nil)
 	m.Extra = dns.Dedup(m.Extra, nil)
 	w.WriteMsg(m)
